8be2cb8b8a8d55ba84145b3152e3286399032cd6
alf <alfmohenu@outlook.com>
2018-07-03 08:11:21 +0300

    Mobile menu with animations completed.

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,18 +1,24 @@
 import $ from "jquery";
 
 class MobileMenu {
-    constructor(){
+    constructor() {
         this.menuIcon = $(".s-header__menu-icon");
-        this.menuContent = $(".s-header__menu-content");
+        this.headerLogo = $(".s-header__logo");
+        this.headerNavList = $(".s-header-nav__list");
+        this.header = $(".s-header");
+        this.menuIcon = $(".s-header__menu-icon");
         this.events();
     }
 
-    events(){
+    events() {
         this.menuIcon.click(this.toggleMenu.bind(this));
     }
 
     toggleMenu() {
-        this.menuContent.toggleClass("s-header__menu-content--is-visible");
+        this.headerNavList.toggleClass("s-header-nav__list--invisible");
+        this.headerLogo.toggleClass("s-header__logo--invisible");
+        this.header.toggleClass("s-header--invisible");
+        this.menuIcon.toggleClass("s-header__menu-icon--close-x");
     }
 }
 
